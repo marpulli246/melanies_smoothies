f96fbfd69d5051c64417cc1f85bb2aa6846d3828
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2025-02-03 13:28:52 -0500

    Update streamlit_app.py
    json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 
 #display smoothiefroot nutrition facts
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json)
